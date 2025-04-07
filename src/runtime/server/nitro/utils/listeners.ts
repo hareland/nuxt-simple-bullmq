b2e0c7b45438123dbef4eb2963284b8751ace8d7
hareland <packagist@proton.me>
2025-04-07 15:11:19 +0200

    add feat for listeners

---
+++ src/runtime/server/nitro/utils/listeners.ts
@@ -0,0 +1,29 @@
+import type { ZodSchema } from 'zod'
+import type { EventListener } from '../types'
+import { emitEvent, emitValidatedEvent } from '~/src/runtime/server/nitro/utils/queue'
+
+// todo: for future usage when defining file based listeners for auto registration and "triggering"
+// const listener = defineEventListener('listenToMe', {
+//   queueName: 'default',
+//   schema: z.object({ userId: z.string() }),
+//   async handle({ data }) {},
+// })
+//
+// // here handle should also be typed correctly
+// await listener.handle({ data: { userId: 'test' } })
+
+export const defineEventListener = <Schema extends ZodSchema>(
+  eventName: string,
+  options: Omit<EventListener<Schema>, 'eventName' | 'trigger'>,
+): EventListener<Schema> => {
+  return {
+    eventName,
+    ...options,
+    async trigger(payload) {
+      if (!options.schema) {
+        return emitEvent(eventName, payload, options)
+      }
+      return emitValidatedEvent(eventName, options.schema, payload, options)
+    },
+  }
+}
